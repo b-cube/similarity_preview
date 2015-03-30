412d2084ffde4e06e4be6ebdf8c756c9f1741019
Soren Scott <soren.scott@nsidc.org>
2015-03-30 00:51:22 -0600

    shift to static for lazy flask, difflib option, pprint xml

--- preview/similarities.py
+++ preview/similarities.py
@@ -1,17 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import flask
 from flask import Flask
 from flask import abort
 from flask import render_template
 from flask import request
 from flask import Response
+from flask import Markup
 app = Flask(__name__)
 
 import os
 import glob
 import json
 import operator
+import difflib
+from itertools import chain
 from lxml import etree
-from xml.sax.saxutils import escape
+# from xml.sax.saxutils import escape
 
 
 @app.route("/view")
@@ -29,26 +34,46 @@ def get_page():
     source_digest = request.args.get('source', '')
     compare_digest = request.args.get('compare', '')
 
-    digests = []
-    if not source_digest and not compare_digest:
-        files = glob.glob('preview/similarities/*_similarities.txt')
-        digests = [os.basename(f).replace('_similarities.txt', '') for f in files]
+    # digests = []
+    # if not source_digest and not compare_digest:
+    #     files = glob.glob('preview/similarities/*_similarities.txt')
+    #     digests = [os.basename(f).replace('_similarities.txt', '') for f in files]
 
     source_response = ''
     if source_digest:
-        similarities = parse_similarities('preview/similarities/%s_similarity.txt' % source_digest)
-        source_response = parse_response('preview/responses/%s.json' % source_digest)
+        similarities = parse_similarities(
+            'preview/similarities/%s_similarities.txt' % source_digest,
+            'desc'
+        )
+        # add the identification info for the first ten
+        similarities = append_identities(similarities[:10])
+
+        source_response = parse_response(
+            'preview/responses/%s_identified.json' % source_digest,
+            False
+        )
 
     compare_response = ''
     if compare_digest:
-        compare_response = parse_response('preview/responses/%s.json' % compare_digest)
+        compare_response = parse_response(
+            'preview/responses/%s_identified.json' % compare_digest,
+            False
+        )
+
+    diff = ''
+    if source_response and compare_response:
+        diff = difflib.HtmlDiff().make_table(source_response.split('\n'),
+                                             compare_response.split('\n'),
+                                             '',
+                                             '',
+                                             True, 4)
 
     return render_template(
         'index.html',
-        digests=digests,
-        source=source_response,
+        source_response=source_response,
         compare_response=compare_response,
-        similarities=similarities
+        similarities=similarities,
+        diff=diff
     )
 
 
@@ -86,11 +111,24 @@ def get_similarity(digest):
     return flask.jsonify(sorted=parsed_sim)
 
 
+def append_identities(tuples):
+    # digest, similarity
+    new_sims = []
+    for t in tuples:
+        with open('preview/responses/%s_identified.json' % t[0], 'r') as f:
+            data = json.loads(f.read())
+        identity = data.get('identity', {})
+        d = {'id': t[0], 'sim': t[1]}
+        d = dict(chain(d.items(), identity.items()))
+        new_sims.append(d)
+    return new_sims
+
+
 def parse_similarities(sim_path, sort_order='asc'):
     with open(sim_path, 'r') as f:
         data = f.readlines()
     data = [d.split(',') for d in data if d]
-    data = {d[1].strip(): d[0].strip() for d in data[1:] if d}
+    data = {d[1].strip(): "%.3f" % float(d[0].strip()) for d in data[1:] if d}
 
     sorted_sims = sorted(data.items(), key=operator.itemgetter(1))
     if sort_order == 'desc':
@@ -104,21 +142,19 @@ def parse_response(digest_path, html_escape=True):
 
     content = data['content'].encode('unicode_escape')
     try:
-        xml = etree.fromstring(content)
+        parser = etree.XMLParser(
+            encoding='utf-8',
+            remove_blank_text=True,
+            recover=True
+        )
+        xml = etree.fromstring(content, parser=parser)
     except Exception as ex:
         raise ex
     text = etree.tostring(xml, pretty_print=True)
 
     # html encode it
     if html_escape:
-        html_escape_table = {
-            '"': "&quot;",
-            "'": "&apos;"
-        }
-        new_lines = []
-        for line in text.split('\n'):
-            new_lines.append(escape(line, html_escape_table))
-        return '\n'.join(new_lines)
+        text = Markup.escape(text)
 
     # just return the pretty-printed xml
     return text
